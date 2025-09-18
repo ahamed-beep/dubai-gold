14751878a8febee7cb1fbc671969207381c885cf
ahamed-beep <muhamedahamed251@gmail.com>
2025-09-18 06:11:21 -0700

    update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
           />
           <div className="ml-3">
             <div className="flex flex-col">
-              <div className=" text-[10px] md:text-xl font-bold text-amber-800 leading-tight">
+              <div className=" text-[12px] md:text-xl font-bold text-amber-800 leading-tight">
                 DUBAI
               </div>
               <div className="text-base font-semibold text-amber-700">
